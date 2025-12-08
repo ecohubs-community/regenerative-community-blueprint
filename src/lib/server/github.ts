4066a9b5ad2e08163d612d655021149b9e35e027
st3v3y <stefan.lessle@mediakular.com>
2025-12-08 14:14:25 -0500

    refactor: Remove deprecated getGitHubClient function and streamline Octokit initialization

--- src/lib/server/github.ts
+++ src/lib/server/github.ts
@@ -1,16 +1,4 @@
 import { Octokit } from '@octokit/rest';
-import { GITHUB_TOKEN } from '$env/static/private';
-
-export function getGitHubClient(): Octokit {
-	const token = GITHUB_TOKEN;
-	if (!token) {
-		throw new Error('GITHUB_TOKEN environment variable is not set');
-	}
-
-	return new Octokit({
-		auth: token
-	});
-}
 
 export function getOctokit(token: string): Octokit {
 	return new Octokit({
